2c75b3ee9fe18675ae9a2524e1b483d7976f01ef
Justin Chou <justinjchou@gmail.com>
2020-04-10 06:52:35 -0700

    string manipulation using hashmaps. CTCI problem

--- hr-stringmanipulation.js
+++ hr-stringmanipulation.js
@@ -1,19 +1,46 @@
+let a = "c d e"
+let a1 = a.split(' ');
+let b = "a b c"
+let b1 = b.split(' ');
+var NUMBER_LETTERS = 26;
+
 function makeAnagram(a, b) {
-    // create hashmap to track the number of letters in the string
-    let tracker = {};
-    for (let i = 0; i < a.length; ++i) {
-        if (!tracker[a[i]]) {
-            tracker[a[i]] = 0;
-        }
-        tracker[a[i]] += 1;
+    let aHash = hashTracker(a);
+    let bHash = hashTracker(b);
+    return getDelta(aHash, bHash);
+}
+
+console.log(makeAnagram(a1, b1));
+
+// this creates a HashMap for the letters in the string
+function hashTracker(s) {
+    let LETTERS = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    let letterHash = LETTERS.split(' ');
+    let hashTracker = {};
+    
+    // // creates the whole alphabet hashmap
+    for (let i = 0; i < 26; ++i) {
+        hashTracker[letterHash[i]] = 0;
     }
-    // check to see if exists in tracker, if doesn't add 1
-    let deletions = 0;
-    for (let i = 0; i < b.length; ++i) {
-        if (tracker[b[i]] <= 0) {
-            deletions += 1;
+    
+    // counts the letters of the alphabet
+    for (let i = 0; i < s.length; ++i) {
+        if (!hashTracker[s[i]]) {
+            hashTracker[s[i]] = 0;
         }
-        tracker[b[i]] -= 1;
+        hashTracker[s[i]] += 1;
     }
-    return deletions;
+    return hashTracker;
+}
+
+function getDelta(a,b) {
+    let LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    let delta = 0;
+    for (let i = 0; i < LETTERS.length; ++i) {
+        let difference = Math.abs(a[LETTERS.charAt(i)] - b[LETTERS.charAt(i)]);
+        // console.log("a: " + a[LETTERS.charAt(i)] + "     b: " + b[LETTERS.charAt(i)] + "     i: " + i);
+        delta += difference;
+    }
+    console.log({delta});
+    return delta;
 }
