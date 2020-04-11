23a09f5012cc8b2ccdbac3e50c40f276d2dc50c8
Justin Chou <justinjchou@gmail.com>
2020-04-11 11:42:47 -0700

    Reorganization into folders.

--- hr-icecreamparlor.js
+++ hr-icecreamparlor.js
@@ -1,22 +1,37 @@
-let cost = 4;
-let money1 = [1,4,5,3,2];
-let money2 = [2,2,4,3];
+let money = 4;
+let cost1 = [1,4,5,3,2];
+let cost2 = [2,2,4,3];
 
+// Brute force solution
 function whatFlavors(cost, money) {
-    money.sort(function (a,b) {
-        return a-b;
-    });
-    let sum = 0;
-    let offset = 0;
+    // money.sort(function (a,b) {
+    //     return a-b;
+    // });
+    
     for (let i = 0; i < money.length; ++i) {
         for (let j = i + 1; j < money.length; ++j) {
-            let placeSum = sum;
-            sum = money[i] + money[i + 1];
-            if (sum === cost) {
-                console.log(money[i] + " " + money[i + 1]);
-            } else if (sum > money) {
-                sum = placeSum;
+            if(money[i] + money[j] === cost) {
+                console.log((i + 1), (j + 1));
+                break;
             }
         }
     }
 }
+
+// whatFlavors(cost, money1);
+
+function whatFlavorsHash(cost, money) {
+    // loop through hashmap and if cJ exists, return. if not ++i and check through again for its complement
+    const map = new Map();
+    for (let i = 0; i < cost.length; i++) {
+        var target = money - cost[i];
+        if (map.has(target)) {
+            console.log(map.get(target), i + 1);
+            break;
+        }
+        map.set(cost[i], i + 1);
+    }
+}
+
+whatFlavorsHash(cost1, money);
+
