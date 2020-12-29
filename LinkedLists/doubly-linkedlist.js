c0da0ed9c7bbe2d3e46b34232de6093f4361aed3
Justin J Chou <justinjchou@gmail.com>
2020-12-29 11:15:32 -0800

    linked lists v1.0

---
+++ LinkedLists/doubly-linkedlist.js
@@ -0,0 +1,13 @@
+var node = {
+    value: val,
+    next: null,
+    previous: null
+}
+
+function DoublyLinkedList() {
+    this.head = null;
+}
+
+DoublyLinkedList.prototype.push = function(val) {
+    
+}
