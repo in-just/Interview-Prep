e7d348c73c38866ffc5432a9bbb23a5689d75933
Justin J Chou <justinjchou@gmail.com>
2020-12-30 09:39:19 -0800

    add method

--- LinkedLists/HWC-linkedlists.js
+++ LinkedLists/HWC-linkedlists.js
@@ -1,4 +1,5 @@
 /* most important part of a linked list is its node structure each node must contain some data and a pointer to the next node in the list.
+https://humanwhocodes.com/blog/2019/01/computer-science-in-javascript-linked-list/
 */
 
 class LinkedListNode {
@@ -9,17 +10,34 @@ class LinkedListNode {
     }
 }
 
-// creating a linked list using the LinkedListNode class
-// 1st node
-const head = new LinkedListNode(12);
-// 2nd node
-head.next = new LinkedListNode(99);
-// 3rd node
-head.next.next = new LinkedListNode(37);
+// class encapsulates struct functionality
+const head = Symbol("head");
 
-// traversing a linked list and printing
-let current = head;
-while (current != null) {
-    console.log(current.data);
-    current = current.next;
+class LinkedList {
+    constructor() {
+        this[head] = null;
+    }
+    // adding new data to the list
+    add(data) {
+        
+        // create a new node
+        const newNode = new LinkedListNode(data);
+
+        // special case: no items in the list yet
+        if (this[head] === null) {
+            // just set head to the new node
+            this[head] = newNode;
+        } else {
+            // start looking at first node
+            let current = this[head];
+
+            // follow `next` links until reach the end
+            while (current.next !== null) {
+                current = current.next;
+            }
+
+            // assign the node into the `next` pointer
+            current.next = newNode;
+        }
+    }
 }
